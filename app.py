8b2fa205aca5caf465738f4e75094e416bf4be73
Mr Douglass <k.dougla01@pudseygrangefield.co.uk>
2019-11-21 11:26:30 +0000

    v0.2

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def index():
 	return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8081, host='0.0.0.0')
+    app.run(debug=True, port=5000, host='127.0.0.1')
