5a166a36eba63db39487346fac4800bdc30c1646
Mr Douglass <k.dougla01@pudseygrangefield.co.uk>
2019-11-22 08:12:53 +0000

    v0.4
    Addition of further iMDB details

--- app.py
+++ app.py
@@ -18,12 +18,46 @@ def movie():
         poster = item['Poster']
         imdbID = item['imdbID']
 
+    #return json_object
+    #return str(items)
     return render_template('movie.html', items=items)
 
+@app.route('/info', defaults={'id': 'tt4654462'})
+@app.route('/info/<id>', methods=['POST','GET'])
+def info(id):
+    apikey = '6f6c977'
+    imdb_search = id
+    r = requests.get('http://www.omdbapi.com/?apikey='+apikey+'&i='+imdb_search)
+    json_object = r.json()
+
+    #items = json_object['ID']
+
+    #for item in json_object:
+    poster = json_object['Poster']
+    title = json_object['Title']
+    rated = json_object['Rated']
+    director = json_object['Director']
+    runtime = json_object['Runtime']
+    plot = json_object['Plot']
+    released = json_object['Released']
+
+    ratings = json_object['Ratings']
+
+    for rating in ratings:
+        source = rating['Source']
+        value = rating['Value']
+
+    #return json_object
+    return render_template('info.html', ratings=ratings, poster=poster, title=title, rated=rated, director=director, runtime=runtime, plot=plot, released=released)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route('/infosearch')
+def infosearch():
+	return render_template('info-search.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='127.0.0.1')
